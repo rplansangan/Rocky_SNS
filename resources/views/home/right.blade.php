f88e0d126e37e3cdf38d31951bbab84340f0fd11
kmilana <kmilana.dmundo@gmail.com>
2015-03-18 15:36:06 +0800

    right sidebar

--- resources/views/home/right.blade.php
+++ resources/views/home/right.blade.php
@@ -1,12 +1,34 @@
 <div class="row">
-	<div class="col-lg-12 col-md-12">
+	<div class="col-lg-12 col-md-12 rfriends">
 		<legend>Friends</legend>
 		<nav>
 			<ul>
-				<li><a href="#"><img src="{{ URL::asset('assets/images/browncat.png') }}" width="30px" height="30px" class="img-circle"> Supername number 1</a></li>
-				<li><a href="#"><img src="{{ URL::asset('assets/images/nermal.png') }}" width="30px" height="30px" class="img-circle"> Supername number 2</a></li>
-				<li><a href="#"><img src="{{ URL::asset('assets/images/odie.png') }}" width="30px" height="30px" class="img-circle"> Supername number 3</a></li>
-				<li><a href="#"><img src="{{ URL::asset('assets/images/odiedif.png') }}" width="30px" height="30px" class="img-circle"> Supername number 4</a></li>
+				<li><a href="#"><img src="{{ URL::asset('assets/images/browncat.png') }}" width="30px" height="30px"> Supername number 1</a></li>
+				<li><a href="#"><img src="{{ URL::asset('assets/images/nermal.png') }}" width="30px" height="30px"> Supername number 2</a></li>
+				<li><a href="#"><img src="{{ URL::asset('assets/images/odie.png') }}" width="30px" height="30px"> Supername number 3</a></li>
+				<li><a href="#"><img src="{{ URL::asset('assets/images/odiedif.png') }}" width="30px" height="30px"> Supername number 4</a></li>
+			</ul>
+		</nav>
+	</div>
+	<div class="col-lg-12 col-md-12 rhotdeals">
+		<legend>Hot Deals</legend>
+		<nav>
+			<ul>
+				<li><a href="#"><img src="{{ URL::asset('assets/images/browncat.png') }}" width="30px" height="30px"> Supername number 1</a></li>
+				<li><a href="#"><img src="{{ URL::asset('assets/images/nermal.png') }}" width="30px" height="30px"> Supername number 2</a></li>
+				<li><a href="#"><img src="{{ URL::asset('assets/images/odie.png') }}" width="30px" height="30px"> Supername number 3</a></li>
+				<li><a href="#"><img src="{{ URL::asset('assets/images/odiedif.png') }}" width="30px" height="30px"> Supername number 4</a></li>
+			</ul>
+		</nav>
+	</div>
+	<div class="col-lg-12 col-md-12 rhotitems">
+		<legend>Hot Items</legend>
+		<nav>
+			<ul>
+				<li><a href="#"><img src="{{ URL::asset('assets/images/browncat.png') }}" width="30px" height="30px"> Supername number 1</a></li>
+				<li><a href="#"><img src="{{ URL::asset('assets/images/nermal.png') }}" width="30px" height="30px"> Supername number 2</a></li>
+				<li><a href="#"><img src="{{ URL::asset('assets/images/odie.png') }}" width="30px" height="30px"> Supername number 3</a></li>
+				<li><a href="#"><img src="{{ URL::asset('assets/images/odiedif.png') }}" width="30px" height="30px"> Supername number 4</a></li>
 			</ul>
 		</nav>
 	</div>
