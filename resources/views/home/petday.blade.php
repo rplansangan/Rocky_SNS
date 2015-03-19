c3d562efd4ff888bd2c63a7d1d13d56ee366648d
kmilana <kmilana.dmundo@gmail.com>
2015-03-19 17:23:05 +0800

    pet of the day page

---
+++ resources/views/home/petday.blade.php
@@ -0,0 +1,26 @@
+<div class="col-sm-12 col-xs-12 col-md-12 col-lg-12 pet-day-container">
+	<div class="col-sm-12 col-xs-12 col-md-12 col-lg-12 pd-content">
+		<h1>PET OF THE DAY</h1>
+
+		<div class="col-sm-12 col-xs-12 col-md-5 col-lg-5 pd-photo">
+			<a href="#"><img src="{{ URL::asset('assets/images/garfielking.png') }}"></a>
+		</div>
+
+		<div class="col-sm-12 col-xs-12 col-md-7 col-lg-7 pd-name">
+			<a href="#"><h2>Mypet Name</h2></a>
+		</div>
+
+		<div class="col-lg-12 pd-infos">
+			<ul class="pd-infos-text">
+				<li><span>Type:</span> Cat</li>
+				<li><span>Breed:</span> Domestic Short Hair</li>
+				<li><span>Age:</span> 2 years old</li>
+				<li><span>Gender:</span> Male</li>
+				<li><span>Likes:</span> Lasagna, squeaky toys, mouse, tummy rub</li>
+				<li><span>Dislikes:</span> Odie, rubber duckies</li>
+				<li><span>Foods:</span> Lasagna, pizza</li>
+				<li><span>Owner:</span> Jon Arbuckle</li>
+			</ul>
+		</div>
+	</div>
+</div>
