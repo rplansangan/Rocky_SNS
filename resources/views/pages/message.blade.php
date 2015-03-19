790dba9e7d7cacda956e414b5222a4aa46eec9c3
kmilana <kmilana.dmundo@gmail.com>
2015-03-19 15:30:40 +0800

    message page
    this is where the page get redirected after you clicked the signup
    button in the landing page

--- resources/views/pages/message.blade.php
+++ resources/views/pages/message.blade.php
@@ -1,13 +1,25 @@
 @extends('master')
 @section('site_title')
-Welcome to Rocky The Superdog
+Confirm Rocky The Superdog Account
 @stop
 
 @section('content')
-<div class="col-sm-12 col-xs-12 col-md-12 col-lg-12 land-content">
+<div class="col-sm-12 col-xs-12 col-md-12 col-lg-12 msg-content">
 	<div class="container">
-		asd
+		<div class="col-sm-12 col-xs-12 col-md-12 col-lg-12 msg-text">
+			<h1>Final Step...</h1>
+
+			<p>Please check the email we sent you to verify your account.</p>
+			<p>Didn't receive the email? <a href="#">click here</a> to resend.</p>
+		</div>
 	</div>
 </div>
+
+<style>
+.footer{
+	position: absolute;
+	bottom: 0;
+}
+</style>
 
 @stop
