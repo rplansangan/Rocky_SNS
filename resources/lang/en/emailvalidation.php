39c6e57b2eccd288df8a7131af496f26b2acee82
kmilana <kmilana.dmundo@gmail.com>
2015-03-19 14:53:29 +0800

    email message

--- resources/lang/en/emailvalidation.php
+++ resources/lang/en/emailvalidation.php
@@ -1,5 +1,10 @@
 <?php
 return array(
-		'message_header' => 'this is a test header',
-		'message_body_0' => 'this is a test body :id/:hash'			
+		'message_header' => 'Confirm Your Rocky The Superdog Account',
+		'message_body_0' => 'Hi :name, thank you for joining our community.
+Please Verify this email address to complete your Rocky The Superdog account by clicking the link below:
+
+:hash
+
+If you\'re not :name or didn\'t request verification, you can ignore this email. :route'		
 );
