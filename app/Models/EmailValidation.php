51bfb2dfa92800fd34b16ef2e73b1c2256b045cb
RAP <rplansangan@gmail.com>
2015-03-23 16:38:52 +0800

    Revert "Revert "model documentation and relationships""
    This reverts commit 36a461dfc6ed2b0545eb099dfb32827c85ce9750.

--- app/Models/EmailValidation.php
+++ app/Models/EmailValidation.php
@@ -7,10 +7,21 @@ class EmailValidation extends Model {
 
 	use SoftDeletes;
 	
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
 	protected $table = 'email_validations';
 	
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
 	protected $fillable = array('registration_id', 'hash');
 	
+	// RELATIONSHIPS
 	public function registration() {
 		return $this->belongsTo('SNS\Models\Registration');
 	}
--- app/Models/Registration.php
+++ app/Models/Registration.php
@@ -8,10 +8,24 @@ class Registration extends Model {
 
 	use SoftDeletes;
 	
+	/**
+	 *
+	 * @var string
+	 */
 	protected $primaryKey = 'registration_id';
 	
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
 	protected $table = 'registrations';
 	
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
 	protected $fillable = array('last_name', 'first_name', 'birth_date', 'gender', 'address_line1', 'address_line2',
 								'city', 'zip', 'state', 'country', 'phone_country_code', 'phone_area_code', 'phone_number',
 								'alias', 'email_address', 'is_deactivated', 'last_deactivated', 'last_profile_update', 'user_id', 'is_validated');
@@ -36,6 +50,7 @@ class Registration extends Model {
 			
 	);
 	
+	// RELATIONSHIPS
 	public function emailValidation() {
 		return $this->hasOne('SNS\Models\EmailValidation');
 	}
