a2767e7e9a4c98a420b10296eeee07dee54c22d6
Mhar Bucad <mhar0987@live.com>
2015-03-24 15:57:47 +0800

    ito na!!!!!!

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -70,24 +70,7 @@ class HomeController extends Controller {
 
 	public function test(Request $request){
 		$input = array_except($request->all(), array('_token'));
-		?>
-		<li class="media">
-			<div class="media-left">
-				<a href="#">
-					<img class="media-object" src="{{ URL::asset('assets/images/browncat.png') }}" withd="64px" height="64px" alt="profile picture">
-				</a>
-			</div>
-			<div class="media-body">
-				<h4 class="media-heading">Superdog</h4>
-				<small class="media-heading">March 18 2015 12:00:00 am</small>
-				<p><?php echo $input['post_message']?></p>
-				<p><a href="#"><i class="fa fa-thumbs-up"></i> 1 Likes....</a> <a href="javascript:void(0)">Comment</a></p>
-				<form method="POST" action="{{ url('login') }}"  role="form" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-					<input type="hidden" name="_token" value="{{ csrf_token() }}">
-					<textarea max="500" name="post_message" class="comment-box" placeholder=" Say Something..."></textarea>
-				</form>	
-			</div>
-		</li>
-		<?php
+		$data['info'] = $input;
+		return view('ajax.post' , $data);
 	}
 }
