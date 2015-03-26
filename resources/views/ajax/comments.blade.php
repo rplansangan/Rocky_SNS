b1d87ed0bde31ceacda0f7adee394c9c448006a3
kmilana <kmilana.dmundo@gmail.com>
2015-03-26 17:49:40 +0800

    comment css

--- resources/views/ajax/comments.blade.php
+++ resources/views/ajax/comments.blade.php
@@ -1,11 +1,11 @@
 <li class="media">
     <div class="media-left">
       <a href="#">
-        <img class="media-object"  width="64px" height="64px" src="{{ URL::asset('assets/images/jon.jpg') }}" alt="...">
+        <img class="media-object"  width="30px" height="30px" src="{{ URL::asset('assets/images/jon.jpg') }}" alt="...">
       </a>
     </div>
-    <div class="media-body">
-      <h4 class="media-heading">{{ $comment->user->first_name }} {{ $comment->user->last_name }}</h4>
+    <div class="media-body com-name">
+      <a href="{{ route('profile.showProfile', Auth::id()) }}"><span class="media-heading">{{ $comment->user->first_name }} {{ $comment->user->last_name }} </span></a>
       {{ $comment->comment_message }}
     </div>
   </li>
