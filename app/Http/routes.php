b9c8c687002e013e044f12680561db44903740ec
kmilana <kmilana.dmundo@gmail.com>
2015-03-30 15:52:44 +0800

    removed conflict in routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -90,14 +90,10 @@ Route::get('petlist/{id}', array(
 	'as' => 'profile.petlist',
 	'uses' => 'ProfileController@petlist'
 ));
-<<<<<<< HEAD
 Route::get('profile/{id}/pet/{pet_id}', array(
 	'as' => 'profile.showPetProfile',
 	'uses' => 'ProfileController@showPetProfile'
 ));
-=======
-
->>>>>>> parent of a49df14... 1
 Route::post('likes/set/{post_id}', array(
 	'as' => 'likes.set',
 	'uses' => 'PostsController@setLike'
