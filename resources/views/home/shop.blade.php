4bf99a999e35bb471b502075980e5c8e295c10ee
kmilana <kmilana.dmundo@gmail.com>
2015-04-06 12:45:38 +0800

    Shop Page contents added

---
+++ resources/views/home/shop.blade.php
@@ -0,0 +1,15 @@
+<div class="adshop-content col-sm-12 col-xs-12 col-lg-12 col-md-12">
+	<legend><h1>ADS</h1></legend>
+
+	<div class="col-sm-12 col-xs-12 col-lg-6 col-md-6 adshop-container">
+		<div class="col-sm-12 col-xs-12 col-lg-12 col-md-12 adshop-img">
+			<img src="{{ URL::asset('assets/images/ad11.jpg') }}">
+		</div>
+	</div>
+
+	<div class="col-sm-12 col-xs-12 col-lg-6 col-md-6 adshop-container">
+		<div class="col-sm-12 col-xs-12 col-lg-12 col-md-12 adshop-img">
+			<img src="{{ URL::asset('assets/images/ad22.jpg') }}">
+		</div>
+	</div>
+</div>
