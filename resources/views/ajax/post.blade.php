3b8d3ac4f52475ad4157682383c70a513ebe6fd4
Mhar Bucad <mhar0987@live.com>
2015-04-07 14:16:29 +0800

    haha

--- resources/views/ajax/post.blade.php
+++ resources/views/ajax/post.blade.php
@@ -19,8 +19,8 @@
 		@if(isset($image))
 		<img class="col-sm-12" src="{{ route('files.get.image', array($message->user_id, $image->image_id)) }}">
 		@endif
-		<i class="fa fa-thumbs-up"></i>&nbsp;<span class="like-counter">{{ count($like) }}</span> 
-		<i class="fa fa-comment"></i>&nbsp;<span class="comment-counter">{{ count($comments) }}</span>&nbsp; 
+		<i class="fa fa-thumbs-up"></i>&nbsp;<span class="like-counter">{{{ isset($include_script) ? 0 : count($like) }}} </span> 
+		<i class="fa fa-comment"></i>&nbsp;<span class="comment-counter">{{{ isset($include_script) ? 0 : count($comments) }}}</span>&nbsp; 
 		<a class="nf-like comment-like" href="#" value="{{ $message->post_id }}" value2="{{ route('likes.set', array($message->post_id)) }}" value3="{{ csrf_token() }}">{{ isLike($like) }} &#8226;</a>
 		<a class="nf-like comment-form" href="javascript:void(0)">Comment</a>
 		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 comment-form comment-form-hidden">
