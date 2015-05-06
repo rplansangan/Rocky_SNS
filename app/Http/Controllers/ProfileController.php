c9b8ac12f3372292f5bd24e11c3a6849aa2b67fb
kmilana <kmilana.dmundo@gmail.com>
2015-05-06 17:52:26 +0800

    :paperclip:

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -14,6 +14,7 @@ use Carbon\Carbon;
 use Illuminate\Support\Facades\Storage;
 use SNS\Libraries\Facades\StorageHelper;
 use SNS\Models\User;
+use SNS\Models\Registration;
 
 class ProfileController extends Controller {
 	
