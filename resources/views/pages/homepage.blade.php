23a7851934076ed931b9957deda298a4eb3f6a9f
Zairus81 <eleazar_pdl@yahoo.com>
2015-05-11 17:16:47 +0800

    Message should go here
    preparation for placement of messages

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -10,7 +10,7 @@ Rocky the Superdog
 		<div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 left">
 			@include('home.left')
 		</div>
-		<div class="col-lg-6 col-lg-6 col-md-3 col-sm-12 col-xs-12 middle">
+		<div id = "home_middle" class="col-lg-6 col-lg-6 col-md-3 col-sm-12 col-xs-12 middle">
 			@include('home.middle', array('newsfeed' => $newsfeed))
 		</div>
 		<div class="col-lg-3 col-lg-6 col-md-3 col-sm-12 col-xs-12 right">
