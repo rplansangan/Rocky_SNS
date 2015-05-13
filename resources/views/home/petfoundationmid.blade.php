2f287dd5b96b6dfe5b78b30227e1a2d679ebe4a6
kmilana <kmilana.dmundo@gmail.com>
2015-05-13 16:51:43 +0800

    :+1:

--- resources/views/home/petfoundationmid.blade.php
+++ resources/views/home/petfoundationmid.blade.php
@@ -4,23 +4,23 @@
 			<h2>Page Foundation List</h2>
 		</div>
 		<div class="col-sm-4 text-center" style="margin-top:10px">
-			<a href="#"><img width="100%"src="{{ URL::asset('assets/images/found1.jpg') }}"></a>
+			<a href="#"><img width="100%"src="{{ URL::asset('assets/images/foundation1.jpg') }}"></a>
 			<a href="#">Pet Foundation 1</a>
 		</div>
 		<div class="col-sm-4 text-center" style="margin-top:10px">
-			<a href="#"><img width="100%"src="{{ URL::asset('assets/images/ad22.jpg') }}"></a>
+			<a href="#"><img width="100%"src="{{ URL::asset('assets/images/foundation2.jpg') }}"></a>
 			<a href="#">Pet Foundation 2</a>
 		</div>
 		<div class="col-sm-4 text-center" style="margin-top:10px">
-			<a href="#"><img width="100%"src="{{ URL::asset('assets/images/found3.jpg') }}"></a>
+			<a href="#"><img width="100%"src="{{ URL::asset('assets/images/foundation3.jpg') }}"></a>
 			<a href="#">Pet Foundation 3</a>
 		</div>
 		<div class="col-sm-4 text-center" style="margin-top:10px">
-			<a href="#"><img width="100%"src="{{ URL::asset('assets/images/found5.jpg') }}"></a>
+			<a href="#"><img width="100%"src="{{ URL::asset('assets/images/foundation4.jpg') }}"></a>
 			<a href="#">Pet Foundation 4</a>
 		</div>
 	</div>
-	<div class="row text-center">
+	<!--<div class="row text-center">
 		<a href="#">Load More...</a>
-	</div>
+	</div>-->
 </div>
