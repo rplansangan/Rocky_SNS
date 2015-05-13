fd007a93726f883f9421f173f63f4232fb240c3b
Mhar Bucad <mhar0987@live.com>
2015-05-13 16:20:46 +0800

    :+1:

--- resources/views/home/petfoundationmid.blade.php
+++ resources/views/home/petfoundationmid.blade.php
@@ -1,3 +1,26 @@
 <div class="col-xs-12 petfoundation-mid">
-	TESTTTTT
+	<div class="row">
+		<div class="page-header">
+			<h2>Page Foundation List</h2>
+		</div>
+		<div class="col-sm-4 text-center" style="margin-top:10px">
+			<a href="#"><img width="100%"src="{{ URL::asset('assets/images/found1.jpg') }}"></a>
+			<a href="#">Pet Foundation 1</a>
+		</div>
+		<div class="col-sm-4 text-center" style="margin-top:10px">
+			<a href="#"><img width="100%"src="{{ URL::asset('assets/images/ad22.jpg') }}"></a>
+			<a href="#">Pet Foundation 2</a>
+		</div>
+		<div class="col-sm-4 text-center" style="margin-top:10px">
+			<a href="#"><img width="100%"src="{{ URL::asset('assets/images/found3.jpg') }}"></a>
+			<a href="#">Pet Foundation 3</a>
+		</div>
+		<div class="col-sm-4 text-center" style="margin-top:10px">
+			<a href="#"><img width="100%"src="{{ URL::asset('assets/images/found5.jpg') }}"></a>
+			<a href="#">Pet Foundation 4</a>
+		</div>
+	</div>
+	<div class="row text-center">
+		<a href="#">Load More...</a>
+	</div>
 </div>
