29322d2ac7d283cf36752d28a202ccfed6c9d842
kmilana <kmilana.dmundo@gmail.com>
2015-05-20 17:40:14 +0800

    :+1:

--- resources/views/home/right.blade.php
+++ resources/views/home/right.blade.php
@@ -6,7 +6,6 @@
 		<span class="glyphicon glyphicon-hand-left"></span></h3>
 		<div class="found-imgs">
 			<img src="{{ URL::asset('assets/images/found1.jpg') }}" width="250px" height="185px">
-			<img src="{{ URL::asset('assets/images/found2.jpg') }}" width="250px" height="185px">
 		</div>
 		<button type="button" class="btn btn_found" data-toggle="modal" data-target="#misfo" data-type="Found" data-advertisetype="I Found A Pet"  data-title="I Found A Pet" data-id="" data-action="" >I Found A Pet</button>
 	</div>
@@ -17,7 +16,6 @@
 		<span class="glyphicon glyphicon-hand-left"></span></h3>
 		<div class="missing-imgs">
 			<img src="{{ URL::asset('assets/images/missing1.jpg') }}" width="250px" height="185px">
-			<img src="{{ URL::asset('assets/images/missing2.jpg') }}" width="250px" height="185px">
 		</div>
 		<button type="button" class="btn btn_missing" data-toggle="modal" data-target="#misfo" data-type="Missing" data-advertisetype="Find My Pet"  data-title="Missing Pet" data-id="" data-action="" >Find My Pet</button>
 	</div>
