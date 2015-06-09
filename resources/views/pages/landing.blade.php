388f5f762d005c7ac81adedfd65e6d702757e0ab
Mhar Bucad <mhar0987@live.com>
2015-06-09 16:58:20 +0800

    :+1:

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -197,10 +197,10 @@ Welcome to Rocky The Superdog
 
 	<div class="funny-videos col-sm-12 col-xs-12 col-md-3 col-lg-3 text-left" >
 		<h3>FUNNY VIDEOS</h3>
-		<div class="row" style="padding:0px 2px">
+		<div class="row" style="padding:2px 2px">
 			<iframe width="100%" height="225" src="https://www.youtube.com/embed/N9oxmRT2YWw" frameborder="0" allowfullscreen></iframe>
 		</div>
-		<div class="row" style="padding:0px 2px">
+		<div class="row" style="padding:2px 2px">
 			<iframe width="100%" height="225" src="https://www.youtube.com/embed/SpkSejI8OQE" frameborder="0" allowfullscreen></iframe>
 		</div>
 
