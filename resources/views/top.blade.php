bdb4c8a01669131824dc425f99b4a73303faa3c7
Mhar Bucad <mhar0987@live.com>
2015-06-26 15:17:10 +0800

    :+1:  menu

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -6,10 +6,18 @@
         <div class="col-md-3 col-lg-3 logo">
           <a href="{{ route('index') }}"><img class="img-responsive" src="{{ URL::asset('assets/images/rlysmall.png') }}"></a>
         </div>
-        <div class="col-md-9 col-lg-9 land-nav-menu text-right">
+        <div class="col-md-9 col-lg-9 land-nav-menu">
           <ul>
             <li><a class="parent" href="#">PET LOVERS</a></li>
-            <li><a class="parent" href="#">PET NEEDS</a></li>
+            <li class="has-sub">
+              <a class="parent" href="#">PET NEEDS</a>
+              <ul>
+                <li><a href="">VETS</a></li>
+                <li><a href="">GROOMERS</a></li>
+                <li><a href="">WALKERS</a></li>
+                <li><a href="">SHOPS</a></li>
+              </ul>
+            </li>
             <li><a class="parent" href="#">PET VIDEOS</a></li>
             <li><a class="parent" href="#">PET FOUNDATIONS</a></li>
             <li><a class="parent" href="#">ROCKY RANGER</a></li>
