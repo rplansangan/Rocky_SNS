b96b508db1b36b73e9bd47b1511fa8de84c43a6e
kmilana <kmilana.dmundo@gmail.com>
2015-07-10 13:47:31 +0800

    add pet modal done

--- resources/views/include/formlogin.blade.php
+++ resources/views/include/formlogin.blade.php
@@ -149,20 +149,17 @@ $(document).ready(function() {
                 <h4 class="modal-title" id="addpetModalLabel">Add Your Pets</h4>
             </div>
             <div class="modal-body">
-                <div class="col-sm-12 col-xs-12 col-md-12 col-lg-12 register-page">
-                <div class="container-fluid">
-                    <div class="col-sm-12 col-xs-12 col-md-8 col-lg-8 col-md-offset-2 col-lg-offset-2 land-main-cont register-main" >
-                        <div class="row">
-                            <div class="page-header" style="padding:10px 20px 10px 20px">
-                                <h2>Add Your Pet(s)</h2>
-                                <small class="help-block">Please complete the form to add your pet</small>
-                            </div>
+                <div class="register-page">
+                    <div class="register-main">
+                        <div class="page-header" style="padding-left: 10px;">
+                            <h2 style="margin-top: 0;">Add Your Pet</h2>
+                            <small class="help-block">Please complete the form to add your pet</small>
                         </div>
-                        <div class="row">
-                            <form method="POST" action="{{ route('register.petRegister', Auth::id()) }}" class="form-horizontal col-sm-12  col-lg-12 col-md-12  col-xs-12 reg " role="form" enctype="multipart/form-data">
+                        <div>
+                            <form method="POST" action="{{ route('register.petRegister', Auth::id()) }}" class="form-horizontal reg " role="form" enctype="multipart/form-data">
                                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">Profile Picture</label>
+                                    <label class="col-sm-3 control-label">Profile Picture</label>
                                     <div class='col-sm-8'>
                                         <input type='file' name="petfile" class="custom-file-input" />
                                     </div>
@@ -171,25 +168,25 @@ $(document).ready(function() {
                                     <div class="col-sm-3 col-sm-offset-2 view-image-here"></div>
                                 </div>
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">Pet Name:</label>
+                                    <label class="col-sm-3 control-label">Pet Name:</label>
                                     <div class="col-sm-8">
                                         <input type="text" name="pet_name" class="form-control" placeholder="Name">
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">How to call this pet:</label>
+                                    <label class="col-sm-3 control-label">How to call this pet:</label>
                                     <div class="col-sm-8">
                                         <input type="text" name="pet_call_attn" class="form-control" placeholder="Name">
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">Rocky Tag Number:</label>
+                                    <label class="col-sm-3 control-label">Rocky Tag Number:</label>
                                     <div class="col-sm-8">
                                         <input type="text" name="rocky_tag_no" class="form-control" placeholder="Rocky Tag Number">
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">Animal Type:</label>
+                                    <label class="col-sm-3 control-label">Animal Type:</label>
                                     <div class='col-sm-8'>
                                         <select id="sel-ani-type" name="pet_type" class="form-control">
                                             <option></option>
@@ -206,13 +203,13 @@ $(document).ready(function() {
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">Breed:</label>
+                                    <label class="col-sm-3 control-label">Breed:</label>
                                     <div class="col-sm-8">
                                         <input type="text" name="breed" class="form-control" placeholder="Breed">
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">Estimated Birthdate:</label>
+                                    <label class="col-sm-3 control-label">Estimated Birthdate:</label>
                                     <div class='col-sm-8'>
                                         <input type="date" class="form-control" id="pet_bday" name="pet_bday">
                                     </div>
@@ -233,7 +230,7 @@ $(document).ready(function() {
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">Weight in lb:</label>
+                                    <label class="col-sm-3 control-label">Weight in lb:</label>
                                     <div class="col-sm-3">
                                         <input type="text" name="weight" class="form-control" placeholder="Weight">
                                     </div>
@@ -243,19 +240,19 @@ $(document).ready(function() {
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">Food:</label>
+                                    <label class="col-sm-3 control-label">Food:</label>
                                     <div class="col-sm-8">
                                         <input type="text" name="food" class="form-control" placeholder="eg: Dog Food">
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">Food Brand:</label>
+                                    <label class="col-sm-3 control-label">Food Brand:</label>
                                     <div class="col-sm-8">
                                         <input id="fld-food-brand" type="text" name="brand" class="form-control" placeholder="Food Brand" disabled>
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">Food Style:</label>
+                                    <label class="col-sm-3 control-label">Food Style:</label>
                                     <div class='col-sm-8'>
                                         <select name="food_style" class="form-control">
                                             <option></option>
@@ -265,51 +262,50 @@ $(document).ready(function() {
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">Feeding Interval:</label>
+                                    <label class="col-sm-3 control-label">Feeding Interval:</label>
                                     <div class="col-sm-8">
                                         <input type="text" name="feeding_interval" class="form-control" placeholder="eg: twice a day">
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">Feeding Time:</label>
+                                    <label class="col-sm-3 control-label">Feeding Time:</label>
                                     <div class="col-sm-8">
                                         <input type="text" name="feeding_time" class="form-control" placeholder="eg: 8am and 5pm">
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">Behavior</label>
+                                    <label class="col-sm-3 control-label">Behavior</label>
                                     <div class="col-sm-8">
                                         <input id="fld-pet-behavior" type="text" name="behavior" class="form-control" disabled>
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">Likes:</label>
+                                    <label class="col-sm-3 control-label">Likes:</label>
                                     <div class="col-sm-8">
                                         <input type="text" name="pet_likes" class="form-control" placeholder="Likes">
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">Dislikes:</label>
+                                    <label class="col-sm-3 control-label">Dislikes:</label>
                                     <div class="col-sm-8">
                                         <input type="text" name="pet_dislikes" class="form-control" placeholder="Dislikes">
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">Identifying Marks:</label>
+                                    <label class="col-sm-3 control-label">Identifying Marks:</label>
                                     <div class='col-sm-8 add-pet-file-div'>
                                         <input type='file' name="identifying_marks[]" class="ident_marks col-sm-6" />
                                         <input type="text" name="identifying_marks_desc[]" class="col-sm-6" placeholder="Identifying Mark Description" />
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <div class='col-sm-8 col-sm-offset-2 Tracker123text-right'>
-                                        <input type='submit' class="btn" value="Done"/>
+                                    <div class='col-sm-8 col-sm-offset-3 Tracker123 text-right'>
+                                        <input type='submit' class="btn" value="Add"/>
                                     </div>
                                 </div>
                             </form>
                         </div>
                     </div>
-                </div>
                 </div>
             </div>
         </div>
