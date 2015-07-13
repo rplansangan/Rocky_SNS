5be02653e551586d5c8734b78f568b90a62f8eb2
kmilana <kmilana.dmundo@gmail.com>
2015-07-13 17:32:21 +0800

    profile

--- resources/views/pages/inside/profile.blade.php
+++ resources/views/pages/inside/profile.blade.php
@@ -3,10 +3,10 @@
 		@if(isset($profile->prof_pic))
 			<img src="{{ mediaSrc($profile->prof_pic->image_path, $profile->prof_pic->image_name , $profile->prof_pic->image_ext) }}" class="profile-pic">
 		@else
-			<img src="{{ URL::asset('assets/images/default-pic.png') }}" class="profile-pic">
+			<img src="{{ URL::asset('assets/images/default-pic.png') }}" class="profile-dp">
 		@endif
 		<h3>{{ $profile->first_name.' '.$profile->last_name }} <br><small class="text-muted">Have 5 Pets</small></h3>
-		<a href="#">Send a message</a>
+		<a href="#" class="send-msg">Send a message</a>
 		<br clear="all">
 	</div>
 </div>
