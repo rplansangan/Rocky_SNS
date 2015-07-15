03b7a8f2956c03f7dbf5c29db949a92abb3a22a0
RAP <rplansangan@gmail.com>
2015-07-15 12:00:08 +0800

    fixes
    fixed null profile picture exception
    fixed improper user id retrieval

--- app/Libraries/Cache/Initialize.php
+++ app/Libraries/Cache/Initialize.php
@@ -43,55 +43,44 @@ class Initialize {
      * return void
      */
     public function initAuth() {
+    	$params = $this->auth->user();
+    	
+    	$params->load([
+    			'registration' => function($q) {
+    				$q->addSelect(['registration_id', 'user_id', 'first_name', 'last_name']);
+    			},
+    			'prof_pic' => function($q) {
+    				$q->where('pet_id', 0);
+    				$q->addSelect(['image_id', 'user_id', 'image_path', 'image_name', 'image_ext']);
+    			}
+    	]);
+    	
+    	$user = [
+	    			'user_id' => $params->user_id,
+	    			'first_name' => $params->registration->first_name,
+	    			'last_name' => $params->registration->last_name
+				];
+    	
+    	if(!is_null($params->prof_pic)) {
+    		$profile_pic = [
+	    		'profile_picture_path' => $params->prof_pic->image_path . '/' . $params->prof_pic->image_name,
+	    		'profile_picture_ext' => $params->prof_pic->image_ext
+    		];
+    	} else {
+    		$profile_pic = null;
+    	}
+    	
+    	$profile = json_encode(array_merge($user, [$profile_pic]));
+    	
     	if($this->redis !== false) {
-	    	if(!$this->cache->exists($this->keysProfile . $this->auth->id())) {
-		        $params = $this->auth->user();
-		        
-				$params->load([
-					'registration' => function($q) {
-						$q->addSelect(['registration_id', 'user_id', 'first_name', 'last_name']);
-					},
-		            'prof_pic' => function($q) {
-		                $q->where('pet_id', 0);
-		                $q->addSelect(['image_id', 'user_id', 'image_path', 'image_name', 'image_ext']);
-		            }
-				]);
-				
-				$profile = json_encode([
-								'user_id' => $params->user_id,
-								'first_name' => $params->registration->first_name,
-								'last_name' => $params->registration->last_name,
-								'profile_picture_path' => $params->prof_pic->image_path . '/' . $params->prof_pic->image_name,
-								'profile_picture_ext' => $params->prof_pic->image_ext
-							]);
-	
+	    	if(!$this->cache->exists($this->keysProfile . $this->auth->id())) {	
 				$this->cache->set($this->keysProfile . $params->user_id, $profile);
 				
 				// set expiration
 				$this->cache->expire($this->keysProfile . $params->user_id, $this->expSession);
 	    	}
     	} else {
-    	if(!$this->cache->get($this->keysProfile . $this->auth->id())) {
-		        $params = $this->auth->user();
-		        
-				$params->load([
-					'registration' => function($q) {
-						$q->addSelect(['registration_id', 'user_id', 'first_name', 'last_name']);
-					},
-		            'prof_pic' => function($q) {
-		                $q->where('pet_id', 0);
-		                $q->addSelect(['image_id', 'user_id', 'image_path', 'image_name', 'image_ext']);
-		            }
-				]);
-				
-				$profile = json_encode([
-								'user_id' => $params->user_id,
-								'first_name' => $params->registration->first_name,
-								'last_name' => $params->registration->last_name,
-								'profile_picture_path' => $params->prof_pic->image_path . '/' . $params->prof_pic->image_name,
-								'profile_picture_ext' => $params->prof_pic->image_ext
-							]);
-	
+    		if(!$this->cache->get($this->keysProfile . $this->auth->id())) {	
 				$this->cache->put($this->keysProfile . $params->user_id, $profile, $this->keysProfile);
 	    	}
     	}
