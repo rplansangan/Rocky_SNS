302cc6394f0c46e79cb5e68118e80ec6cb478366
Mhar Bucad <mhar0987@live.com>
2015-07-16 14:31:32 +0800

    hey

--- resources/views/ajax/test.blade.php
+++ resources/views/ajax/test.blade.php
@@ -27,66 +27,51 @@
 			{!! $post_message !!}
 
 			@if(isset($image))
-			@if(stristr($image['image_mime'], 'image/'))
-			<div class="image-content">
-				<img src="{{  mediaSrc($image['image_path'], $image['image_name'] , $image['image_ext'])  }}" class="img-responsive img-thumbnail">
-			</div>
-			@else
-			<div class="video-content">
-				<div>
-					<img src="{{ mediaSrc($image['image_path'], $image['image_name'].'_thumb' , 'jpg') }}" class="img-responsive img-thumbnail">
+				@if(stristr($image['image_mime'], 'image/'))
+				<div class="image-content">
+					<img src="{{  mediaSrc($image['image_path'], $image['image_name'] , $image['image_ext'])  }}" class="img-responsive img-thumbnail">
 				</div>
-				<a href="#" class="play"><img src="{{ URL::asset('assets/img/play.png') }}" class="img-responsive"></a>
-			</div>
-			@endif
+				@else
+				<div class="video-content">
+					<div>
+						<img src="{{ mediaSrc($image->image_path, $image->image_name.'_thumb' , 'jpg') }}" class="img-responsive img-thumbnail">
+					</div>
+					<a href="javascript:void(0);" route="{{ Route('get.video') }}" src="{{ mediaSrc($image->image_path, $image->image_name , $image->image_ext) }}" class="play"><img src="{{ URL::asset('assets/img/play.png') }}" class="img-responsive"></a>
+				</div>
+				@endif
 			@endif
 		</div>
 		<!-- END NEWSFEED CONTENT -->
 		<!-- COMMENT SECTION -->
 		<div class="row newsfeed-bottom">
 			<div>
-				<a href="#"><img src="{{ URL::asset('assets/img/like.png') }}"><span class="like">26</span></a>
+				@if(isLike($like))
+				<a href="javascript:void(0);" route="{{ Route('is.liked') }}" post-id="{{ $post_id}}" destination="{{ $user_id }}" class="clickLike text-center" like-image="{{ URL::asset('assets/img/like.png') }}" unlike-image="{{ URL::asset('assets/img/unlike.png') }}"><img src="{{ URL::asset('assets/img/like.png') }}"><span class="like">{{ count($like) }}</span></a>
+				@else
+				<a href="javascript:void(0);" route="{{ Route('is.liked') }}" post-id="{{ $post_id}}" destination="{{ $user_id }}" class="clickLike text-center" like-image="{{ URL::asset('assets/img/like.png') }}" unlike-image="{{ URL::asset('assets/img/unlike.png') }}"><img src="{{ URL::asset('assets/img/unlike.png') }}"><span class="unlike">{{ count($like) }}</span></a>
+				@endif
 			</div>
 			<div>
-				<a href="javascript:void(0);" id="comment-down"><img src="{{ URL::asset('assets/img/comment.png') }}"></a>
+				<a href="javascript:void(0);"  class="comment-down" post-id="{{ $post_id }}" route="{{ Route('get.comment') }}" ><img src="{{ URL::asset('assets/img/comment.png') }}"></a>
 			</div>
 		</div>
 		<!-- END COMMENT SECTION -->
 	</div>
 	<div class="comment-area">
-		<div class="text-left loading-dots">
+		<div class="text-left loading-dots hidden">
 			<a href="#"><span>...</span></a>
 		</div>
-		<div class="comment-container">
-			<div class="col-lg-1">
-				<a href="#"><img src="{{ URL::asset('assets/img/puggy.png') }}" class="profile-pic"></a>
-			</div>
-			<div class="col-lg-11">
-				<div class="row">
-					<div class="col-lg-6 text-left nopad">
-						<a href="#"><span>Bull</span></a>
-					</div>
-					<div class="col-lg-6 text-right nopad">
-						<h6><small>26 Minutes ago</small></h6>
-					</div>
-				</div>
-				<div class="row">
-					<div class="comment-message">
-						<p>I feel good so good</p>
-					</div>
-				</div>
-			</div>
-			<br clear="all">
+		<div class="comment_loading_area">
 		</div>
 		@if(Auth::check())
 		<div class="reply-textarea">
 			<div class="col-lg-12 nopad">
-				@if(isset($profile->prof_pic))
-				<a href="#" class="arrow_right"><img src="{{ mediaSrc($profile->prof_pic->image_path, $profile->prof_pic->image_name , $profile->prof_pic->image_ext) }}" class="img-thumbnail profile-pic"></a>
+				@if(isset($user_data['profile_picture_path']))
+				<a href="#" class="arrow_right"><img src="{{ mediaSrcAlt($user_data['profile_picture_path'], $user_data['profile_picture_ext']) }}" class="img-thumbnail profile-pic"></a>
 				@else
 				<a href="#" class="arrow_right"><img src="{{ URL::asset('assets/img/prof.png') }}" class="img-thumbnail profile-pic"></a>
 				@endif
-				<textarea class="form-control"></textarea>
+				<textarea class="form-control insertComment"  post-user-id="{{ $user_id }}" post-id="{{ $post_id }}" route="{{ Route('insert.comment') }}" placeholder="Write a comment"></textarea>
 			</div>
 			<br clear="all">
 		</div>
